16d7d1c3df5d2b43b70791d2a19a27b7aaa9c194
Heck <97088365+0zero4u@users.noreply.github.com>
2026-02-11 01:32:39 +0530

    Update MicroStrategy.js

--- strategies/MicroStrategy.js
+++ strategies/MicroStrategy.js
@@ -25,7 +25,7 @@ class MicroStrategy {
         this.COOLDOWN_MS = 2000;
 
         // --- VELOCITY CONFIG ---
-        this.SPIKE_PERCENT = 0.000006;   // 0.017%
+        this.SPIKE_PERCENT = 0.0000012;   // 0.017%
         this.SPIKE_WINDOW_MS = 30;
 
         this.assets = {};
