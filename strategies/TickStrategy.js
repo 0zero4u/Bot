0d0015d6413ba34a89b421c19ccdbea681d6480e
Heck <97088365+0zero4u@users.noreply.github.com>
2026-02-11 19:12:40 +0530

    Update TickStrategy.js

--- strategies/TickStrategy.js
+++ strategies/TickStrategy.js
@@ -11,7 +11,7 @@ class TickStrategy {
         // --- STRATEGY PARAMETERS ---
         this.DECAY_ALPHA = 0.5;
         this.WELFORD_ALPHA = 0.02;
-        this.ENTRY_Z = 3.0;
+        this.ENTRY_Z = 4.0;
         this.EXIT_Z = 0.5;
         this.MIN_NOISE_FLOOR = 0.05;
         this.WARMUP_TICKS = 600; 
