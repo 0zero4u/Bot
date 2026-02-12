ac216f9d333312d903a6d3052c056d9e24137957
Heck <97088365+0zero4u@users.noreply.github.com>
2026-02-12 16:08:34 +0530

    Update LeadStrategy.js

--- strategies/LeadStrategy.js
+++ strategies/LeadStrategy.js
@@ -13,7 +13,7 @@ class LeadStrategy {
 
         // --- CONFIGURATION ---
         this.WARMUP_MS = 60000;
-        this.WINDOW_MS = 20;             
+        this.WINDOW_MS = 30;             
         this.IMBALANCE_THRESHOLD = 0.30; 
         
         // --- VOLATILITY CONFIG (Time Based) ---
@@ -23,7 +23,7 @@ class LeadStrategy {
 
         // --- ADAPTIVE THRESHOLD ---
         this.QUANTILE_RANK = 0.9995;      
-        this.BUFFER_SIZE = 60000;        
+        this.BUFFER_SIZE = 20000;        
         this.UPDATE_INTERVAL_MS = 5000;  
         this.MIN_THRESHOLD_FLOOR = 4.0;  
         this.RESET_THRESHOLD_RATIO = 0.8; 
