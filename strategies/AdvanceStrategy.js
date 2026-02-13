130ca512b4e1a7b5050e856a334eed1d1e400df9
Heck <97088365+0zero4u@users.noreply.github.com>
2026-02-14 03:53:19 +0530

    Update AdvanceStrategy.js

--- strategies/AdvanceStrategy.js
+++ strategies/AdvanceStrategy.js
@@ -44,12 +44,12 @@ class AdvanceStrategy {
         this.WARMUP_MS = 30000; // 30 Seconds
         
         // Z-SCORE SETTINGS
-        this.Z_SCORE_THRESHOLD = 1.2;    
+        this.Z_SCORE_THRESHOLD = 1.0;    
         this.MIN_GAP_THRESHOLD = 0.0004; 
         
         // BURST SETTINGS
         this.MIN_BURST_VELOCITY = 0.0001; 
-        this.DOMINANCE_RATIO = 0.65;       
+        this.DOMINANCE_RATIO = 0.53;       
 
         // LAG TRACKER SETTINGS
         this.LAG_CATCHUP_RATIO = 0.5;
