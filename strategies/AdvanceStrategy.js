10b452ac2065ecd3e75365cbec1457e7b82eae59
Heck <97088365+0zero4u@users.noreply.github.com>
2026-02-15 12:34:10 +0530

    Update AdvanceStrategy.js

--- strategies/AdvanceStrategy.js
+++ strategies/AdvanceStrategy.js
@@ -65,7 +65,7 @@ class AdvanceStrategy {
         
         // Z-SCORE SETTINGS
         this.Z_SCORE_THRESHOLD = 1.0;    
-        this.MIN_GAP_THRESHOLD = 0.00035; 
+        this.MIN_GAP_THRESHOLD = 0.0006; 
         
         // TRAILING STOP & TAKE PROFIT
         this.TRAILING_PERCENT = 0.035; 
