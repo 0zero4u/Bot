0b6b29fe531549fde2e6d12b86042a3bc3483753
Heck <97088365+0zero4u@users.noreply.github.com>
2026-02-15 17:24:22 +0530

    Update AdvanceStrategy.js

--- strategies/AdvanceStrategy.js
+++ strategies/AdvanceStrategy.js
@@ -50,7 +50,7 @@ class AdvanceStrategy {
         this.DEPLETION_RATIO = 2.0; 
 
         // --- RISK SETTINGS ---
-        this.ENTRY_BUFFER_TICKS = 3;   
+        this.ENTRY_BUFFER_TICKS = 4;   
         this.TRAILING_PERCENT = 0.035; 
         this.TP_PERCENT = 0.0007; 
         this.LOCK_DURATION_MS = 5000;    
