75e53cb32840a788a8d8049c97237f028d3d9cb3
Heck <97088365+0zero4u@users.noreply.github.com>
2026-02-16 01:32:17 +0530

    Update AdvanceStrategy.js

--- strategies/AdvanceStrategy.js
+++ strategies/AdvanceStrategy.js
@@ -50,7 +50,7 @@ class AdvanceStrategy {
         this.DEPLETION_RATIO = 2.0; 
 
         // --- RISK SETTINGS ---
-        this.ENTRY_BUFFER_TICKS = 10;   
+        this.ENTRY_BUFFER_TICKS = 6;   
         this.TRAILING_PERCENT = 0.02500; 
         // Bracket Take Profit removed per request
         this.LOCK_DURATION_MS = 5000;    
