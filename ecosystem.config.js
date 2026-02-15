5d61f889a4ff4c22518fda6350087c14862439b3
Heck <97088365+0zero4u@users.noreply.github.com>
2026-02-15 12:23:26 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,5 @@
-
 module.exports = {
   apps: [
-    {
-      name: "market-listener",
-      script: "./market_listener.js",
-      cwd: "/home/ubuntu/trading-bot/Bot",
-      restart_delay: 5000
-    },
     {
       name: "delta-trader",
       script: "./trader.js",
