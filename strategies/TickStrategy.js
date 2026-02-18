5332ad6c9132fa8f6ba354d0fa05b6d81318998e
Heck <97088365+0zero4u@users.noreply.github.com>
2026-02-18 09:10:24 +0530

    Update TickStrategy.js

--- strategies/TickStrategy.js
+++ strategies/TickStrategy.js
@@ -35,7 +35,7 @@ class TickStrategy {
         this.REGIME_FLOOR = 0.95;
 
         // 3. The Risk Leash
-        this.TRAILING_PERCENT = 0.035; 
+        this.TRAILING_PERCENT = 0.05; 
         
         // 4. Cooldown
         this.COOLDOWN_MS = 30000;
